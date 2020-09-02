056da599b2bc195b4c27b92842b563be05ac65d5
Curtis <curtismensah48@gmail.com>
2020-09-02 18:33:49 +0000

    allow blank fields for course fields

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -11,26 +11,26 @@ class Course( models.Model):
     # lecturer_name = models.CharField(max_length=100, default='',blank=True)
 
     course_content_one = models.TextField()
-    course_content_two = models.TextField(null=True)
-    course_content_three = models.TextField(null=True)
-    course_content_four = models.TextField(null=True)  
-    course_content_five = models.TextField(null=True)
+    course_content_two = models.TextField(null=True, blank=True)
+    course_content_three = models.TextField(null=True, blank=True)
+    course_content_four = models.TextField(null=True, blank=True)  
+    course_content_five = models.TextField(null=True, blank=True)
 
     date = models.DateField()
 
-    duration = models.IntegerField(help_text='Enter number of hours', null=True)
+    duration = models.IntegerField(help_text='Enter number of hours', null=True, blank=True)
 
 
-    video1_url = models.CharField(max_length=255, null=True)
-    video2_url = models.CharField(max_length=255, null=True)
-    video3_url = models.CharField(max_length=255, null=True)
+    video1_url = models.CharField(max_length=255, null=True, blank=True)
+    video2_url = models.CharField(max_length=255, null=True, blank=True)
+    video3_url = models.CharField(max_length=255, null=True, blank=True)
 
 
-    resource1_url = models.CharField(max_length=255, null=True)
-    resource2_url = models.CharField(max_length=255, null=True)
-    resource3_url = models.CharField(max_length=255, null=True)
+    resource1_url = models.CharField(max_length=255, null=True, blank=True)
+    resource2_url = models.CharField(max_length=255, null=True, blank=True)
+    resource3_url = models.CharField(max_length=255, null=True, blank=True)
 
-    is_required = models.BooleanField(default=True, null=True)
+    is_required = models.BooleanField(default=True, null=True, blank=True)
 
     def __str__(self):
         return self.title
